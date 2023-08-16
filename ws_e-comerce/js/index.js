bfc695a837ba42f6bfc48cdfc44e4f6ac881959f
Gonzalo Flamenci <gonzaloflamenci4@gmail.com>
2023-08-16 03:06:09 -0300

    Tarea terminar el totalizador de precios

--- ws_e-comerce/js/index.js
+++ ws_e-comerce/js/index.js
@@ -2,44 +2,67 @@ import { products } from './data.js';
 
 const $ = document;
 
-function createProductCardTemplate(thumbnail, title, description, price, idx) {
-  const productCardTemplate = `
-  <div class="col-md-4 mb-4">
-  <div class="card">
-  <img class="card-img-top" src="${thumbnail}" height="300" alt="Miniatura de producto ${title}">
-  <div class="card-body">
-  <h5 class="card-title">${title}</h5>
-  <p class="card-text">${description}</p>
-  <p class="card-text">$${price}</p>
-  <button id="addToCartBTN${idx}" class="btn btn-primary">Add to Cart</button>
-  </div>
-  </div>
-  </div>`;
-  return productCardTemplate;
-}
+const cart = [];
 
-function render() {
-  const productsGalery = $.getElementById('products_galery');
-  products.forEach((product, idx) => {
-    const productCardTemplate = createProductCardTemplate(
-      product.thumbnail,
-      product.title,
-      product.description,
-      product.price,
-      idx
-    );
-    productsGalery.innerHTML += productCardTemplate;
-    const btn = $.getElementById(`addToCartBTN${idx}`);
-    btn.onclick = addToCart;
-    console.log(
-      '🚀 ~ file: index.js:34 ~ products.forEach ~ btn:',
-      btn.__proto__
-    );
+const productsGalery = $.getElementById('products_galery');
+productsGalery.addEventListener('click', (e) => {
+  const node = e.target;
+  if (node.classList.contains('btn')) {
+    addToCart(node.dataset.id);
+  }
+});
+
+function renderProducts() {
+  const template = $.querySelector('#template-galery-productItem').content;
+  const fragment = $.createDocumentFragment();
+  products.forEach((product) => {
+    template.querySelector('img').src = product.thumbnail;
+    template.querySelector(
+      'img'
+    ).alt = `Miniatura de producto ${product.title}`;
+    template.querySelector('h5').textContent = product.title;
+    template.querySelector('.product-description').textContent =
+      product.description;
+    template.querySelector('.product-price').textContent = `$ ${product.price}`;
+    template.querySelector('button').setAttribute('data-id', product.id);
+    const clone = template.cloneNode(true);
+    fragment.appendChild(clone);
   });
+  productsGalery.appendChild(fragment);
 }
 
-function addToCart(idx) {
-  console.log('Producto', idx);
+function addToCart(id) {
+  const productFound = products.find((product) => product.id === parseInt(id));
+  const productCartList = $.getElementById('productCartList');
+  cart.push(productFound);
+
+  const template = $.querySelector('#template-cart-productItem').content;
+  const fragment = $.createDocumentFragment();
+
+  cart.forEach((product) => {
+    template.querySelector('.cart-product-title').textContent = product.title;
+    template.querySelector(
+      '.cart-product-price'
+    ).textContent = `$ ${product.price}`;
+    template.querySelector('.cart-product-amount').textContent = '1';
+    template.querySelector('button').setAttribute('data-id', product.id);
+    const clone = template.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  productCartList.innerHTML = '';
+  productCartList.appendChild(fragment);
+
+  const initialValue = 0;
+
+  const cartPrice = cart.reduce(
+    (acc, product) => acc + product.price,
+    initialValue
+  );
+  console.log('🚀 ~ file: index.js:56 ~ addToCart ~ cartPrice:', cartPrice);
 }
 
-render();
+function main() {
+  renderProducts();
+}
+
+main();
